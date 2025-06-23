5fefc3d1823981b4b0e400f7d1bcab358776252f
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2025-06-23 11:55:50 -0400

    adding trivial theorems

--- CoveringSpacesProject/SectionOne.lean
+++ CoveringSpacesProject/SectionOne.lean
@@ -1,4 +1,15 @@
 import Mathlib
 /-%%
 This is section one of the Covering Spaces project.
+The first theorem is a test.
+\begin{theorem}[TheoremOne]\label{TheoremOne}\lean{TheoremOne}\leanok
+This is the first theorem.
+\end{theorem}
+%%-/
+theorem TheoremOne : True := by
+  trivial
+/-%%
+\begin{proof}
+The proof is trivial.
+\end{proof}
 %%-/
--- CoveringSpacesProject/SectionTwo.lean
+++ CoveringSpacesProject/SectionTwo.lean
@@ -2,3 +2,16 @@ import Mathlib
 /-%%
 This is section two of the Covering Spaces project.
 %%-/
+
+/-%%
+\begin{theorem}[TheoremTwo]\label{TheoremTwo}\lean{TheoremTwo}\leanok
+This is the second theorem.
+\end{theorem}
+%%-/
+theorem TheoremTwo : True := by
+  trivial
+/-%%
+\begin{proof}\uses{TheoremOne}
+The proof is trivial, but uses Theorem \ref{TheoremOne}.
+\end{proof}
+%%-/
