3f4dcdb73c3997fcd1cf21dbef189babe2d8ba20
AlexKontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2026-03-17 22:17:40 -0400

    updating toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -18,7 +18,7 @@ lean_lib «CoveringSpacesProject»
 -- the commit to a more recent one.
 --
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.20.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.28.0"
 
 meta if get_config? env = some "dev" then require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4.git" @ "v4.20.0"
+  "https://github.com/leanprover/doc-gen4.git" @ "v4.28.0"
